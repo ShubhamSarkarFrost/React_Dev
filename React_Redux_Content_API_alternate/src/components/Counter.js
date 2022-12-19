b5ab26458ed8ea7365664cdd00fba7f9f6ab55a9
Ronnie <roiecse8@gmail.com>
2022-12-19 09:11:47 -0800

    React Redux Basic Two - 19 - 12 -2022 _09:45_PM_IST

--- React_Redux_Content_API_alternate/src/components/Counter.js
+++ React_Redux_Content_API_alternate/src/components/Counter.js
@@ -6,7 +6,8 @@ const Counter = () => {
 
   const dispatch  =  useDispatch()
   const counter =  useSelector(state => state.counter)
-  const toggleCounterHandler = () => {};
+  const showCounter = useSelector(state => state.showCounter)
+
 
   const incrementHandler = () =>{
        dispatch({type: 'increment'});
@@ -15,12 +16,21 @@ const Counter = () => {
       dispatch({type:'decrement'});
   };
 
+  const increaseHandler =() => {
+      dispatch({type:'increase', value: 5})
+  }
+
+  const toggleCounterHandler =() =>{
+      dispatch({type:'toggle' })
+  }
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+        {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
           <button onClick={incrementHandler}>Increment</button>
+          <button onClick={increaseHandler}>Increment By 5</button>
           <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
