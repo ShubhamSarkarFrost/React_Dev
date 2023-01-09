b2e24d411ff3d2e7bce26aeaed6dc4418a6fd124
Shubham Sarkar <ronniecse8@gmail.com>
2023-01-09 22:48:32 +0530

    React_Dev_React_Redux_Auth_Reducer_10:47_PM_09_01_2022_IST

--- React_Redux_Content_API_alternate/src/components/Counter.js
+++ React_Redux_Content_API_alternate/src/components/Counter.js
@@ -1,27 +1,28 @@
 import { Component } from 'react';
 import {useSelector , useDispatch, connect} from "react-redux";
+import { counterActions } from '../store/index'
 import classes from './Counter.module.css';
 
 const Counter = () => {
 
   const dispatch  =  useDispatch()
-  const counter =  useSelector(state => state.counter)
-  const showCounter = useSelector(state => state.showCounter)
+  const counter =  useSelector(state => state.counter.counter)
+  const showCounter = useSelector(state => state.counter.showCounter)
 
 
   const incrementHandler = () =>{
-       dispatch({type: 'increment'});
+       dispatch(counterActions.increment());
   };
   const decrementHandler = () =>{
-      dispatch({type:'decrement'});
+      dispatch(counterActions.decrement());
   };
 
   const increaseHandler =() => {
-      dispatch({type:'increase', value: 5})
+      dispatch(counterActions.increase(5)) // {type: SOM_UNIQUE_IDENTIFIER, payload : 10}
   }
 
   const toggleCounterHandler =() =>{
-      dispatch({type:'toggle' })
+      dispatch(counterActions.toggle())
   }
 
   return (
--- React_Redux_Content_API_alternate/src/store/index.js
+++ React_Redux_Content_API_alternate/src/store/index.js
@@ -1,10 +1,10 @@
 // import { createStore } from 'redux'
 import { createSlice, configureStore } from '@reduxjs/toolkit'
 
-const initialState = {counter: 0, showCounter: true}
+const initialCountetState = {counter: 0, showCounter: true}
 const counterSlice =  createSlice({
         name: 'counter',
-        initialState: initialState,
+        initialState: initialCountetState,
         reducers: {
             increment(state) {
                 state.counter++;
@@ -13,10 +13,10 @@ const counterSlice =  createSlice({
                 state.counter--;
             },
             increase(state, action) {
-                state.counter = state.counter + action.value
+                state.counter = state.counter + action.payload;
             },
             toggle(state) {
-                state.showCounter = !state.showCounter
+                state.showCounter = !state.showCounter;
             }
         }
      });
@@ -58,8 +58,31 @@ const counterSlice =  createSlice({
 //
 // }
 
+const initialAuthState ={
+    isAuthenticated: false
+}
+
+
+
+
+const authSlice = createSlice({
+         name: 'authentication',
+         initialState:initialAuthState,
+         reducers: {
+             login(state) {
+                 state.isAuthenticated = true;
+             },
+             logout(state) {
+                 state.isAuthenticated = false;
+             }
+         }
+     })
+
 const store = configureStore({
-    reducer: counterSlice.reducer
+    reducer: {counter : counterSlice.reducer , auth: authSlice.reducer}
 })
+
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store
