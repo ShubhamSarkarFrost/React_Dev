6ed0b77b2e9f9549f069632eea365540238e7020
Shubham Sarkar <ronniecse8@gmail.com>
2023-01-30 20:32:18 +0530

    Redux_Refresher+Without_Backend_08:31_PM_30_Januray_2023_IST_Commit_One

--- React_Redux_Advanced_Starting_Project/src/Store/cart-slice.js
+++ React_Redux_Advanced_Starting_Project/src/Store/cart-slice.js
@@ -10,10 +10,11 @@ const cartSlice = createSlice({
      reducers: {
          addItemToCart(state, action) {
              const newItem = action.payload;
-             const existingItem = state.items.find(item => item.id === newItem.id)
+             const existingItem = state.items.find((item) => item.id === newItem.id)
+             state.totalQuantity++;
              if(!existingItem){
                  state.items.push({
-                     itemId: newItem.id,
+                     id: newItem.id,
                      price: newItem.price,
                      quantity: 1,
                      totalPrice: newItem.price,
@@ -26,9 +27,10 @@ const cartSlice = createSlice({
          },
          removeItemFromCart(state, action) {
              const removeItemId = action.payload;
-             const existingItem = state.items.find(item => item.id === removeItemId)
+             const existingItem = state.items.find((item) => item.id === removeItemId)
+             state.totalQuantity--;
              if(existingItem.quantity === 1){
-                 state.items = state.items.filter(item => item.id !== removeItemId)
+                 state.items = state.items.filter((item) => item.id !== removeItemId)
              }else{
                  existingItem.quantity--;
                  existingItem.totalPrice = existingItem.totalPrice - existingItem.price
