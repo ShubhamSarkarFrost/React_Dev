37759651184a5ca62b8394a7bfce4de7211c8f9d
Shubham Sarkar <ronniecse8@gmail.com>
2023-03-07 20:19:32 +0530

    ReactDev-advanced-redux-project-project-ninteen-Complete-08:18_PM_IST_07_MAR_2023

--- React_Redux_Advanced_Starting_Project/src/components/Shop/ProductItem.js
+++ React_Redux_Advanced_Starting_Project/src/components/Shop/ProductItem.js
@@ -1,37 +1,42 @@
+import { useDispatch } from 'react-redux';
+
+import { cartActions } from '../../store/cart-slice';
 import Card from '../UI/Card';
-import { cartActions } from "../../Store/cart-slice";
-import { useDispatch } from "react-redux";
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
-  const { title, price, description, id } = props;
-  const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
+    const { title, price, description, id } = props;
 
+    const addToCartHandler = () => {
+        // and then send Http request
+        // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
 
-  const addItemsHandler = () =>{
-      dispatch(cartActions.addItemToCart({
-          id: id,
-          price: price,
-          title: title,
-      }))
-  }
+        dispatch(
+            cartActions.addItemToCart({
+                id,
+                title,
+                price,
+            })
+        );
+    };
 
-
-  return (
-    <li className={classes.item}>
-      <Card>
-        <header>
-          <h3>{title}</h3>
-          <div className={classes.price}>${price.toFixed(2)}</div>
-        </header>
-        <p>{description}</p>
-        <div className={classes.actions}>
-          <button onClick={addItemsHandler}>Add to Cart</button>
-        </div>
-      </Card>
-    </li>
-  );
+    return (
+        <li className={classes.item}>
+            <Card>
+                <header>
+                    <h3>{title}</h3>
+                    <div className={classes.price}>${price.toFixed(2)}</div>
+                </header>
+                <p>{description}</p>
+                <div className={classes.actions}>
+                    <button onClick={addToCartHandler}>Add to Cart</button>
+                </div>
+            </Card>
+        </li>
+    );
 };
 
 export default ProductItem;
+
