3369d3ad89efd672f03c9d3c47a6441bd24ed233
Ronnie <roiecse8@gmail.com>
2023-06-21 10:49:38 -0700

    React_Dev_NextJS+NewProject:01_11:35_PM_21_06_2023

---
+++ React_Different_Approach_Big_Application/pages/[meetupid]/index.js
@@ -0,0 +1,10 @@
+import {Fragment} from "react";
+
+function MeetUpDetails() {
+    return <Fragment>
+
+
+    </Fragment>
+}
+
+export default MeetUpDetails;
---
+++ React_Different_Approach_Big_Application/pages/new-meetup/index.js
@@ -0,0 +1,14 @@
+//our-domain.com/new-mmetup
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+
+function NewMeetupPage() {
+    function addMeetupHandler(enteredMeetupData){
+        console.log(enteredMeetupData);
+    }
+
+    return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+}
+
+export default NewMeetupPage;
+
