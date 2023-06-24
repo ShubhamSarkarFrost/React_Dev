06465b9c2ac2f15541cae286f68086541e48c9b8
Shubham Sarkar <ronniecse8@gmail.com>
2023-06-24 23:33:48 +0530

    React_Dev_NextJS+NewProject_11:33_PM_21_06_2023

---
+++ React_Different_Approach_Big_Application/components/meetups/MeetUpDetail.js
@@ -0,0 +1,20 @@
+import classes from './MeetupDetail.module.css'
+import {Fragment} from "react";
+
+function MeetUpDetail(props) {
+    return(
+        <Section classname={classes.detail}>
+            <img
+                src={props.image}
+                alt={props.title}
+            />
+            <h1>{props.title}</h1>
+            <address>{props.address}</address>
+            <p>{props.description}</p>
+
+        </Section>
+    )
+
+}
+
+export default MeetUpDetail
--- React_Different_Approach_Big_Application/pages/[meetupid]/index.js
+++ React_Different_Approach_Big_Application/pages/[meetupid]/index.js
@@ -1,10 +1,15 @@
 import {Fragment} from "react";
+import MeetUpDetail from "../../components/meetups/MeetUpDetail";
 
 function MeetUpDetails() {
-    return <Fragment>
-
-
-    </Fragment>
+    return (
+        <MeetUpDetail
+            image='https://img.traveltriangle.com/blog/wp-content/uploads/2019/12/Places-To-Visit-Near-Rome-3_dec.jpg'
+            title='First Meetup'
+            address='Some Street'
+            description='This is the First Meetup Spot'
+        />
+    );
 }
 
-export default MeetUpDetails;
+export default MeetUpDetails
