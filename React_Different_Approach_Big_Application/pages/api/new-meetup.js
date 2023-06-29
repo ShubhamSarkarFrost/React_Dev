d2abfb68021231cf0d122893671e97498aff39bf
Shubham Sarkar <ronniecse8@gmail.com>
2023-06-29 19:22:29 +0530

    React_Dev_NextJS+NewProject-Upto-MongoDB_Integration_completed_Full_29_06_2023_07:23_PM_IST

--- React_Different_Approach_Big_Application/pages/api/new-meetup.js
+++ React_Different_Approach_Big_Application/pages/api/new-meetup.js
@@ -1,25 +1,27 @@
-import {MongoClient} from "mongodb";
+import { MongoClient } from 'mongodb';
 
-// api/new-meetup
-//POST
+// /api/new-meetup
+// POST /api/new-meetup
 
-async function handler(req, res){
-    if(req.method === 'POST'){
-      const data = req.body;
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
 
-     const client = await MongoClient.connect("mongodb+srv://Shubham:M0KsusHVlv3Cjw6v@myfirstdb.6zhlu4f.mongodb.net/?retryWrites=true&w=majority")
-     const db = client.db();
+    const client = await MongoClient.connect(
+      'mongodb+srv://maximilian:TU6WdZF2EjFWsqUt@cluster0.ntrwp.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+    const db = client.db();
 
-     const meetupcollections = db.collection('meetups')
-     const result = await meetupcollections.insertOne(data);
+    const meetupsCollection = db.collection('meetups');
 
-     console.log(result);
+    const result = await meetupsCollection.insertOne(data);
 
-     client.close();
+    console.log(result);
 
-     res.status(201).json({message:'Meetup is Inserted'})
+    client.close();
 
-    }
+    res.status(201).json({ message: 'Meetup inserted!' });
+  }
 }
 
 export default handler;
