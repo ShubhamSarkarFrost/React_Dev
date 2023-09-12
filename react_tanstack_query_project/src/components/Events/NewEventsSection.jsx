66fa4760f8e72cd05c9b70dc7d68c647b7899d27
Ronnie <roiecse8@gmail.com>
2023-09-12 09:44:17 -0700

    React_Dev_REACT_TANSTACK_QUERY_12_09_2023 09:43_PM_IST

---
+++ react_tanstack_query_project/src/components/Events/NewEventsSection.jsx
@@ -0,0 +1,65 @@
+// import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query'
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import EventItem from './EventItem.jsx';
+import {fetchEvents} from "../../util/http.js";
+
+export default function NewEventsSection() {
+  // const [data, setData] = useState();
+  // const [error, setError] = useState();
+  // const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //
+  //
+  //   fetchEvents()
+  //     .then((events) => {
+  //       setData(events);
+  //     })
+  //     .catch((error) => {
+  //       setError(error);
+  //     })
+  //     .finally(() => {
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+const { data, isPending, isError, error } =  useQuery({
+    queryKey: ['events'],
+    queryFn: fetchEvents
+  });
+
+
+  let content;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock title="An error occurred" message={error.info?.message || 'Failed to Fetch Events!!'} />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <section className="content-section" id="new-events-section">
+      <header>
+        <h2>Recently added events</h2>
+      </header>
+      {content}
+    </section>
+  );
+}
