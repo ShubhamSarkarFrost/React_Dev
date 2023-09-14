7339a4c4503dbaebe1f7391b5d88b670318e412e
Ronnie <roiecse8@gmail.com>
2023-09-14 10:08:48 -0700

    React_Dev_REACT_TANSTACK_QUERY_14_09_2023_10:43_PM_IST

--- react_tanstack_query_project/src/components/Events/NewEventsSection.jsx
+++ react_tanstack_query_project/src/components/Events/NewEventsSection.jsx
@@ -26,7 +26,8 @@ export default function NewEventsSection() {
   // }, []);
 const { data, isPending, isError, error } =  useQuery({
     queryKey: ['events'],
-    queryFn: fetchEvents
+    queryFn: fetchEvents,
+    staleTime: 5000,
   });
 
 
