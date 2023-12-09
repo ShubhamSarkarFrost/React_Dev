780f3dc4dc9eeb108bcec345be01612689b7ca4b
Ronnie <roiecse8@gmail.com>
2023-12-09 08:39:59 -0800

    React_Dev_Essentials_Revamped_1_9_12_2023_11:13_PM_IST

--- React_Essentials_01/src/App.jsx
+++ React_Essentials_01/src/App.jsx
@@ -1,19 +1,60 @@
+import { useState } from 'react'
+import CoreConcept from "./components/CoreConcepts.jsx";
+import Header from "./components/Header.jsx";
+
+import reactImg from './assets/react-core-concepts.png';
+import {CORE_CONCEPTS} from "./data.js";
+import {EXAMPLES} from "./data.js";
+import TabButton from "./components/TabButton";
+
+
 function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    const [selectedTopic, setSelectedTopic] = useState('components');
+
+    function handleClick(selectedButton) {
+       setSelectedTopic(selectedButton)
+    }
+
+    return (
+        <div>
+            <Header/>
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept
+                            title={CORE_CONCEPTS[0].title}
+                            description={CORE_CONCEPTS[0].description}
+                            image={CORE_CONCEPTS[0].image}
+                        />
+                        <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description}
+                                     image={CORE_CONCEPTS[1].image}/>
+                        <CoreConcept {...CORE_CONCEPTS[2]}/>
+                        <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description}
+                                     image={CORE_CONCEPTS[3].image}/>
+                    </ul>
+                </section>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton onClick={() => handleClick('components')} >{CORE_CONCEPTS[0].title}</TabButton>
+                        <TabButton onClick={() => handleClick('jsx')}>{CORE_CONCEPTS[1].title}</TabButton>
+                        <TabButton onClick={() => handleClick('props')}>{CORE_CONCEPTS[2].title}</TabButton>
+                        <TabButton onClick={() => handleClick('state')}>{CORE_CONCEPTS[3].title}</TabButton>
+                    </menu>
+                    <div id="tab-content">
+                        <h3>{EXAMPLES[selectedTopic].title}</h3>
+                        <p>{EXAMPLES[selectedTopic].description}</p>
+                        <pre>
+                            <code>
+                                {EXAMPLES[selectedTopic].code}
+                            </code>
+                        </pre>
+                    </div>
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
