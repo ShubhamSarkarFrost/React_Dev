9f61c4c4e9f5ef6f3cdd795bfe31570026bcda19
Ronnie <roiecse8@gmail.com>
2023-12-20 09:18:26 -0800

    React_Dev_REACT_Tic_Tac_Toe_#0002_20_12_2023_10:48_PM_IST

--- React-tic-tac-toe-starting-project/src/App.jsx
+++ React-tic-tac-toe-starting-project/src/App.jsx
@@ -1,16 +1,24 @@
+import {useState} from 'react';
 import Player from './components/Player.jsx';
+import GameBoard from './components/GameBoard.jsx'
+import Log from "./components/log.jsx";
 
 function App() {
+    const [activePlayer,setActivePlayer] = useState('X');
+
+    function handleSelectSquare() {
+        setActivePlayer((currentlyActivePlayer) => currentlyActivePlayer ==='X'? 'O' : 'X')
+    }
     return (
         <main>
             <div id="game-container">
-                <ol id="players">
-                    <Player initialName="Player 1" symbol="X" />
-                    <Player initialName="Player 2" symbol="O" />
+                <ol id="players" className="highlight-player">
+                    <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+                    <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
                 </ol>
-                GAME BOARD
+                <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
             </div>
-            LOG
+            <Log/>
         </main>
     );
 }
--- React-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ React-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,18 +1,31 @@
+import {useState} from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({onSelectSquare,activePlayerSymbol}) {
+
+    const [gameBoard,setGameBoard] = useState(initialGameBoard);
+
+   function handleSquareGameboard(rowIndex,colIndex) {
+       setGameBoard((prevGameboard) => {
+           const updatedGameBoard = [...prevGameboard.map(innerArray => [...innerArray])];
+           updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+           return updatedGameBoard;
+       });
+       onSelectSquare();
+   }
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
+            {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button>{playerSymbol}</button>
+                                <button onClick={() => handleSquareGameboard(rowIndex,colIndex)}>{playerSymbol}</button>
                             </li>
                         ))}
                     </ol>
