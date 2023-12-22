f7fe52006048c0601fce5358549b7329f28ed6ad
Ronnie <roiecse8@gmail.com>
2023-12-22 08:04:19 -0800

    React_Dev_REACT_Tic_Tac_Toe_COMPLETE_22_12_2023_10:48_PM_IST

--- React-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ React-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,31 +1,17 @@
-import {useState} from "react";
-
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-];
-
-export default function GameBoard({onSelectSquare,activePlayerSymbol}) {
-
-    const [gameBoard,setGameBoard] = useState(initialGameBoard);
-
-   function handleSquareGameboard(rowIndex,colIndex) {
-       setGameBoard((prevGameboard) => {
-           const updatedGameBoard = [...prevGameboard.map(innerArray => [...innerArray])];
-           updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-           return updatedGameBoard;
-       });
-       onSelectSquare();
-   }
+export default function GameBoard({ onSelectSquare, board }) {
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => (
+            {board.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button onClick={() => handleSquareGameboard(rowIndex,colIndex)}>{playerSymbol}</button>
+                                <button
+                                    onClick={() => onSelectSquare(rowIndex, colIndex)}
+                                    disabled={playerSymbol !== null}
+                                >
+                                    {playerSymbol}
+                                </button>
                             </li>
                         ))}
                     </ol>
