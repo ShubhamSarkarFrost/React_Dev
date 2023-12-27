70a17b3ca9dd5eb251daa8367984a8ccbae84c45
Shubham Sarkar <ronniecse8@gmail.com>
2023-12-27 23:39:38 +0530

    React_Dev_REACT_TIC-TAC-TOE_Project_Complete_27_12_2023_11:39_PM_IST

--- React-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ React-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,36 +1,22 @@
-import {useState} from "react";
-
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-];
-
-export default function GameBoard({onSelectSquare,activePlayerSymbol}) {
-
-    const [gameBoard,setGameBoard] = useState(initialGameBoard);
-
-   function handleSquareGameboard(rowIndex,colIndex) {
-       setGameBoard((prevGameboard) => {
-           const updatedGameBoard = [...prevGameboard.map(innerArray => [...innerArray])];
-           updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-           return updatedGameBoard;
-       });
-       onSelectSquare();
-   }
-    return (
-        <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => (
-                <li key={rowIndex}>
-                    <ol>
-                        {row.map((playerSymbol, colIndex) => (
-                            <li key={colIndex}>
-                                <button onClick={() => handleSquareGameboard(rowIndex,colIndex)}>{playerSymbol}</button>
-                            </li>
-                        ))}
-                    </ol>
-                </li>
+export default function GameBoard({ onSelectSquare, board }) {
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
             ))}
-        </ol>
-    );
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
--- React-tic-tac-toe-starting-project/src/components/Player.jsx
+++ React-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,30 +1,43 @@
 import { useState } from 'react';
 
-export default function Player({ initialName, symbol, isActive }) {
-    const [ playerName, setPlayerName ] = useState(initialName)
-    const [ isEditing, setIsEditing ] = useState(false);
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
-    function handleEditClick() {
-        setIsEditing((editing) => !editing );
-    }
-
-    function handleChange(event){
-        setPlayerName(event.target.value);
-    }
-
-    let editablePlayerName = <span className="player-name">{playerName}</span>;
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
 
     if (isEditing) {
-        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
+      onChangeName(symbol, playerName);
     }
+  }
 
-    return (
-        <li className={isActive? 'active': undefined}>
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  // let btnCaption = 'Edit';
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    // btnCaption = 'Save';
+  }
+
+  return (
+    <li className={isActive ? 'active' : undefined}>
       <span className="player">
         {editablePlayerName}
-          <span className="player-symbol">{symbol}</span>
+        <span className="player-symbol">{symbol}</span>
       </span>
-            <button onClick={handleEditClick}>{isEditing === false ? 'EDIT':'SAVE'}</button>
-        </li>
-    );
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  );
 }
